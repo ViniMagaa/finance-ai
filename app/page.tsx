09b2788fed5550369650ab709d962651344d6eec
ViniMagaa <vinisantos2008vs@gmail.com>
2024-11-05 23:22:37 -0300

    feat: add login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
 	return (
 		<div className="flex items-center justify-center w-screen">
-			<h1 className="justify-center flex">Hello, World!</h1>
+			<Button>Hello, World!</Button>
 		</div>
 	);
 }
