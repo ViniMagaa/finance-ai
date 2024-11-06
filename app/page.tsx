7a2320051a5f6ec177d4957c40f9b08fc762a203
ViniMagaa <vinisantos2008vs@gmail.com>
2024-11-05 22:50:33 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-	return <h1>Hello, World!</h1>;
+	return (
+		<div className="flex items-center justify-center w-screen">
+			<h1 className="justify-center flex">Hello, World!</h1>
+		</div>
+	);
 }
